91edf5ffd62b4f15365175ea9a4d796f3caba946
Khaled <khaledsahli36@gmail.com>
2023-05-23 00:52:26 +0100

    Collapse Table

---
+++ build/precache-manifest.b17536a8499314a1074e06cea1b7df9f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b70fbba8d641bd76b30e",
+    "url": "/static/js/main.1dace5ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfed511fb6ac847f3e56",
+    "url": "/static/js/2.0e7014d1.chunk.js"
+  },
+  {
+    "revision": "f9ba250a267193d91ec6442322e50d0d",
+    "url": "/index.html"
+  }
+];
